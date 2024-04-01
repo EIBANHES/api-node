ec03adb881525fe0b86787f10c0253be76c888fb
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-01 15:17:32 -0400

    Route Params

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const express = require("express");
 //inicialização do express
 const app = express();
 
+// Método get, raiz
+app.get("/message/:id/:user", (request, response) => {
+  // desestruturação
+  const { id, user } = request.params
+  // devolve resposta
+  response.send(`
+  Id da mensagem: ${id}
+  Para o usuário${user}`
+  )
+});
+
 //porta da aplicação
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
