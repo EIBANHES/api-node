fee110cc7c35b17f6428a21168bf55118de74abc
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-08 14:47:52 -0400

    Aprendendo sobre query params

--- src/server.js
+++ src/server.js
@@ -7,13 +7,18 @@ const app = express();
 // Método get, raiz
 app.get("/message/:id/:user", (request, response) => {
   // desestruturação
-  const { id, user } = request.params
+  const { id, user } = request.params;
   // devolve resposta
   response.send(`
   Id da mensagem: ${id}
-  Para o usuário${user}`
+  Para o usuário: ${user}`
   )
 });
+
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query; // Query params
+  response.send(`Página: ${page}, Mostrar: ${limit}`);
+})
 
 //porta da aplicação
 const PORT = 3333;
