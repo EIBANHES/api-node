f8cc6c33d652039b973358bf8d9e0dc0f01ba321
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-08 15:33:24 -0400

    Criando pasta controller

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+class UsersController {
+  /**
+   * PODE TER ATÉ 5 MÉTODOS
+    * index - GET para listar vários registros.
+    * show - GET para exibir um registro especifico.
+    * create - POST criar um registro.
+    * update - PUT atualizar um registro.
+    * delete - DELETE para remover um registro.
+   */
+}
+
+module.exports = UsersController;
