35569620e8bd130c0c526a5c65e4c2ae0a93c512
Emanuel Barros Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-08 19:29:25 -0400

    Utilizando middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,9 +8,17 @@ const usersRoutes = Router();
 
 const usersController = new UsersController();
 
+function myMiddleware(request, response, next) {
+  console.log("Voce passou pelo middleware.");
+  if (!request.body.isAdmin) {
+    return response.json({ message: "User unathorized" });
+  }
+  next(); // chama a proxima função a ser executada.
+}
+
 // Inicio Rotas
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create);
 
 // Fim Rotas
 
