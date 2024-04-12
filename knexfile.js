9acc23ae3f911a83a6c244dd5664e27266a30631
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-12 14:09:35 -0300

    Migrations de tags e links

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db"),
     },
+    pool : {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb) //habilita a funçao de cascata
+    },
     migrations: {
       directory: path.resolve(
         __dirname,
